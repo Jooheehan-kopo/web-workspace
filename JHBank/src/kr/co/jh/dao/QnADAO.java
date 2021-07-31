8b60294e86ecd1b41b82735c86280d357242a019
Jooheehan-kopo <79953962+Jooheehan-kopo@users.noreply.github.com>
2021-08-01 01:51:53 +0900

    0801

--- JHBank/src/kr/co/jh/dao/QnADAO.java
+++ JHBank/src/kr/co/jh/dao/QnADAO.java
@@ -1,18 +1,54 @@
 package kr.co.jh.dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+import kr.co.jh.util.ConnectionFactory;
 import kr.co.jh.vo.QnAVO;
 
 public class QnADAO {
 	
-	public void insert(QnAVO qna) {
+	public List<QnAVO> list() {
 		
+		List<QnAVO> list= new ArrayList<>();
 		StringBuilder sql = new StringBuilder();
-		sql.append("");
-		sql.append("");
-		sql.append("");
-		sql.append("");
-		sql.append("");
+		sql.append("  SELECT Q_NO,USER_ID, Q_TITLE, Q_CONTENT, TO_CHAR(Q_DATE,'YYYY/MM/DD') AS Q_DATE,  ");		
+		sql.append("  Q_VIEWCNT, COMMENT_NO   FROM QNA  ");
 		
+		try( Connection conn = new ConnectionFactory().getConnection();
+				PreparedStatement pstmt = conn.prepareStatement(sql.toString());
+				) {
+			
+			ResultSet rs = pstmt.executeQuery();
+			
+			while (rs.next()) {
+				int q_no = rs.getInt("q_no");
+				String user_id = rs.getString("user_id");
+				String q_title = rs.getString("q_title");
+				String q_date = rs.getString("q_date");
+				int q_viewcnt = rs.getInt("q_viewcnt");
+				int comment_cnt = rs.getInt("comment_cnt");
+				
+				QnAVO qna = new QnAVO();
+				qna.setQ_no(q_no);
+				qna.setQ_title(q_title);
+				qna.setUser_id(user_id);
+				qna.setCommnet_cnt(comment_cnt);
+				qna.setQ_date(q_date);
+				qna.setQ_viewCnt(q_viewcnt);
+				
+				list.add(qna);
+				
+			}
+			
+		}catch(Exception e){
+			e.printStackTrace();
+			
+		}
+		return list;
 		
 	}
 }
--- JHBank/src/kr/co/jh/vo/QnAVO.java
+++ JHBank/src/kr/co/jh/vo/QnAVO.java
@@ -4,80 +4,119 @@ public class QnAVO {
 
 	private int q_no; //글번호
 	private String q_title;
-	private String q_writer;
-	private String q_regdate;
+	private String user_id;
+	private String q_date;
 	private int q_viewCnt;
 	private String q_content;
+	private int commnet_cnt;
+	
+	private String q_writer;
 	
 	public QnAVO() {
 		super();
 	}
 
-	public QnAVO(int q_no, String q_title, String q_writer, String q_regdate, int q_viewCnt, String q_content) {
+
+	public QnAVO(int q_no, String q_title, String user_id, String q_writer, String q_date, int q_viewCnt,
+			String q_content, int commnet_cnt) {
 		super();
 		this.q_no = q_no;
 		this.q_title = q_title;
+		this.user_id = user_id;
 		this.q_writer = q_writer;
-		this.q_regdate = q_regdate;
+		this.q_date = q_date;
 		this.q_viewCnt = q_viewCnt;
 		this.q_content = q_content;
+		this.commnet_cnt = commnet_cnt;
 	}
+
 
 	public int getQ_no() {
 		return q_no;
 	}
 
+
 	public void setQ_no(int q_no) {
 		this.q_no = q_no;
 	}
+
 
 	public String getQ_title() {
 		return q_title;
 	}
 
+
 	public void setQ_title(String q_title) {
 		this.q_title = q_title;
 	}
+
+
+	public String getUser_id() {
+		return user_id;
+	}
+
+
+	public void setUser_id(String user_id) {
+		this.user_id = user_id;
+	}
+
 
 	public String getQ_writer() {
 		return q_writer;
 	}
 
+
 	public void setQ_writer(String q_writer) {
 		this.q_writer = q_writer;
 	}
 
-	public String getQ_regdate() {
-		return q_regdate;
+
+	public String getQ_date() {
+		return q_date;
 	}
 
-	public void setQ_regdate(String q_regdate) {
-		this.q_regdate = q_regdate;
+
+	public void setQ_date(String q_date) {
+		this.q_date = q_date;
 	}
+
 
 	public int getQ_viewCnt() {
 		return q_viewCnt;
 	}
 
+
 	public void setQ_viewCnt(int q_viewCnt) {
 		this.q_viewCnt = q_viewCnt;
 	}
+
 
 	public String getQ_content() {
 		return q_content;
 	}
 
+
 	public void setQ_content(String q_content) {
 		this.q_content = q_content;
 	}
 
+
+	public int getCommnet_cnt() {
+		return commnet_cnt;
+	}
+
+
+	public void setCommnet_cnt(int commnet_cnt) {
+		this.commnet_cnt = commnet_cnt;
+	}
+
+
 	@Override
 	public String toString() {
-		return "QnAVO [q_no=" + q_no + ", q_title=" + q_title + ", q_writer=" + q_writer + ", q_regdate=" + q_regdate
-				+ ", q_viewCnt=" + q_viewCnt + ", q_content=" + q_content + "]";
+		return "QnAVO [q_no=" + q_no + ", q_title=" + q_title + ", user_id=" + user_id + ", q_writer=" + q_writer
+				+ ", q_date=" + q_date + ", q_viewCnt=" + q_viewCnt + ", q_content=" + q_content + ", commnet_cnt="
+				+ commnet_cnt + "]";
 	}
-	
-	
 	
 	
 	
